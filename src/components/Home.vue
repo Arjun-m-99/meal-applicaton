c2778188bb5d595a1c66d0d0b27db51542000ef8
mallikarjunaGITHUBnexgile <mallikarjuna.akunuri@nexgile.com>
2022-03-30 18:34:16 +0530

    test-vue

---
+++ src/components/Home.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="bg-colour">
+    <div>
+      <button class="bttn" @click="getList">Categories Available</button>
+
+      <button class="bttn" @click="getArea">Area</button>
+    </div>
+
+    <div class="row col-md-12">
+      <div class="alin-l col-md-6">
+        <li class="sie" v-for="item in recipes" :key="item">
+          <mdb-icon icon="chevron-right" />
+          {{ item.strCategory }}
+        </li>
+      </div>
+      <div class="alin-r col-md-6">
+        <li class="sie" v-for="areas in area" :key="areas">
+          {{ areas.strArea }}
+        </li>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Axios from "axios";
+export default {
+  name: "Home",
+
+  data() {
+    return {
+      recipes: "",
+      area: "",
+      ingr: "",
+    };
+  },
+
+  methods: {
+    getList() {
+      Axios.get("https://www.themealdb.com/api/json/v1/1/list.php?c=list").then(
+        (response) => {
+          console.log(response.data.meals);
+          this.recipes = response.data.meals;
+        }
+      );
+    },
+    getArea() {
+      Axios.get("https://www.themealdb.com/api/json/v1/1/list.php?a=list").then(
+        (response) => {
+          console.log(response.data.meals);
+          this.area = response.data.meals;
+        }
+      );
+    },
+  },
+};
+</script>
+<style>
+.bttn {
+  margin: 2%;
+  border: 2px solid black !important;
+  padding: 5px;
+  background-color: #ffe6e6 !important;
+  border-radius: 10px;
+}
+.bttn:hover {
+  background-color: white;
+  transition: 1s;
+  color: #ff4d88 !important;
+}
+.bg-colour {
+  /* background-color: #ffcccc;  */
+  /* background-color: cadetblue !important; */
+  /* background-color: #fcfc03; */
+  background-color: #000000;
+  color: white;
+  margin-top: 9px;
+  text-align: center;
+}
+.alin-l {
+  text-align: left;
+  font-family: 'Times New Roman', Times, serif;
+}
+.alin-r {
+  text-align: left;
+  font-family: 'Times New Roman', Times, serif;
+}
+
+.sie {
+  font-size: 150%;
+  margin-left: 40%;
+}
+</style>
---
+++ src/components/RandomMeal.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+     
+      <h1  >Yummy... Food !</h1>
+      <div class="container">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMkXEWpoUheemO8LyO_1KMo3c8ijQi5-yvLlKHItV1opMyjHsIMLPOwqUC2iTm1VrVW5U&usqp=CAU" style="width:80%"  >
+            <button class="btn" @click="getList">Get Meal</button>
+        </div>
+      <div v-for="item in recipes" :key="item.idMeal">
+      
+      <h2>{{item.strMeal}}</h2>
+      <h2>{{item.idMeal}}</h2>
+     
+      </div>
+  </div>
+</template>
+<script>
+import Axios from 'axios';
+export default {
+    name: 'RandomMeal',
+  data() {
+    return {
+      meal: "",
+      recipes: [],
+      // youtubeVid: ""
+    };
+  },
+  methods: {
+    getList() {
+      Axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then(response => {
+            console.log(response.data);
+          this.recipes = [response.data.meals[0]];
+         
+          // this.youtubeVid = [data.body.meals[0].strYoutube];
+        });
+    },
+  },
+
+
+};
+</script>
+<style>
+h1 {
+    text-align: center;
+    color:gold;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font: size 100%;
+   
+}
+.container{
+    text-align: center;
+    margin: auto;
+}
+
+</style>
+
+
+
+
+
+
+
+
+
